d6ebdaa8b722578032b10c04560e3b6757781579
Bruno Augusto <bruno_agst@hotmail.com>
2023-02-20 22:00:51 -0300

    add digitalread in pins 1 to 8

--- encoder-8x4.ino
+++ encoder-8x4.ino
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+                                                                                            
 const int output1 = 13;
 const int output2 = 12;
 const int output3 = 11;
@@ -13,6 +15,8 @@ const int number7 = 8;
 const int number8 = 9;
 
 void setup() {
+    Serial.begin(9600);
+
     pinMode(output1, OUTPUT);
     pinMode(output2, OUTPUT);
     pinMode(output3, OUTPUT);
@@ -29,47 +33,65 @@ void setup() {
 }
 
 void loop() {
-    if(number1 == 1){
+    const int numberR1 = digitalRead(number1);
+    const int numberR2 = digitalRead(number2);
+    const int numberR3 = digitalRead(number3);
+    const int numberR4 = digitalRead(number4);
+    const int numberR5 = digitalRead(number5);
+    const int numberR6 = digitalRead(number6);
+    const int numberR7 = digitalRead(number7);
+    const int numberR8 = digitalRead(number8);
+
+    if(numberR1 == 1){
+        Serial.println("Number 1 in display");
         digitalWrite(output1, LOW);
         digitalWrite(output2, LOW);
         digitalWrite(output3, LOW);
         digitalWrite(output4, HIGH);
-    } else if(number2 == 1) {
+    } else if(numberR2 == 1) {
+        Serial.println("Number 2 in display");
         digitalWrite(output1, LOW);
         digitalWrite(output2, LOW);
         digitalWrite(output3, HIGH);
         digitalWrite(output4, LOW);
-    } else if(number3 == 1) {
+    } else if(numberR3 == 1) {
+        Serial.println("Number 3 in display");
         digitalWrite(output1, LOW);
         digitalWrite(output2, LOW);
         digitalWrite(output3, HIGH);
         digitalWrite(output4, HIGH);
-    } else  if(number4 == 1) {
+    } else  if(numberR4 == 1) {
+        Serial.println("Number 4 in display");
         digitalWrite(output1, LOW);
         digitalWrite(output2, HIGH);
         digitalWrite(output3, LOW);
         digitalWrite(output4, LOW);
-    } else if(number5 == 1) {
+    } else if(numberR5 == 1) {
+        Serial.println("Number 5 in display");
         digitalWrite(output1, LOW);
         digitalWrite(output2, HIGH);
         digitalWrite(output3, LOW);
         digitalWrite(output4, HIGH);
-    } else if(number6 == 1) {
+    } else if(numberR6 == 1) {
+        Serial.println("Number 6 in display");
         digitalWrite(output1, LOW);
         digitalWrite(output2, HIGH);
         digitalWrite(output3, HIGH);
         digitalWrite(output4, LOW);
-    } else if(number7 == 1) {
+    } else if(numberR7 == 1) {
+        Serial.println("Number 7 in display");
         digitalWrite(output1, LOW);
         digitalWrite(output2, HIGH);
         digitalWrite(output3, HIGH);
         digitalWrite(output4, HIGH);
-    } else if(number8 == 1) {
+    } else if(numberR8 == 1) {
+        Serial.println("Number 8 in display");
         digitalWrite(output1, HIGH);
         digitalWrite(output2, LOW);
         digitalWrite(output3, LOW);
         digitalWrite(output4, LOW);
     } else {
+        Serial.println("Number 0 in display");
         digitalWrite(output1, LOW);
         digitalWrite(output2, LOW);
         digitalWrite(output3, LOW);
